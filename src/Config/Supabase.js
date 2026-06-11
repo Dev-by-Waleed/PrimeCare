c80c612581af6fca01c58b8993d5f18b01f9a2b1
Dev-by-Waleed <walled.a.qazi@gmail.com>
2026-06-11 11:01:08 +0500

    security protection

--- src/Config/Supabase.js
+++ src/Config/Supabase.js
@@ -1,6 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+
 // Create a single supabase client for interacting with your database
-const supabase = createClient('https://gjbehlwydtaawbecuhcx.supabase.co', 'sb_publishable_SkKR1wqL6kw2vX-QHvHRgA_CxM3IBaB')
+const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
 export default supabase;
